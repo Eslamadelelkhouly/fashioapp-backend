e3c53969e068f9e4ea3df543c8f9a910937c01c7
Eslamadelelkhouly <rodyerak@gmail.com>
2025-11-13 02:13:51 +0200

    add view get order & order details

--- fashion-backend/order/views.py
+++ fashion-backend/order/views.py
@@ -1,3 +1,90 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from . import models , serializers
+from notification.models import Notification
+from core.models import Product
+from extra.models import Address
+from rest_framework.permissions import IsAuthenticated
+from django.db import transaction
 # Create your views here.
+
+class AddOrder(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self ,request):
+        data = request.data
+        try:
+            with transaction.atomic():
+                validate_products = []
+                for product_data in data['order_product']:
+                    product = get_object_or_404(Product,id=product_data['product'])
+
+                    validate_products.append({
+                        "product_id":product.id,
+                        "imageUrl":product.imageUrl,
+                        "title":product.title,
+                        "price":product.price,
+                        "quantity":product_data['quantity'],
+                        "size":product_data['size'],
+                        "color":product_data['color'],
+                    })
+                    address = get_object_or_404(Address,id=int(data['address']))
+
+                    order = models.Order.objects.create(
+                        user = request.user,
+                        customer_id = data['customer_id'],
+                        address = address,
+                        order_product = validate_products,
+                        rated = [0],
+                        total_quantity = data['total_quantity'],
+                        subtotal = data['subtotal'],
+                        total = data['total'],
+                        delivery_status = data['delivery_status'],
+                        payment_status = data['payment_status'],
+                    )
+
+                    # create notification
+
+                    title = "Order Successfully Placed"
+                    message = "Your Payment has been successfully and your order has been Successfully Placed"
+
+                    Notification.objects.create(
+                        orderId = order,
+                        title = title,
+                        message = message,
+                        userid = request.user
+                    )
+
+                    order.save()
+                    return Response({"id":order.id},status=status.HTTP_201_CREATED)
+        except Product.DoesNotExist:
+            return Response({"message":"one or more products not found"}, status=status.HTTP_404_NOT_FOUND)
+        except Address.DoesNotExist:
+            return Response({"message":"user addres does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        except KeyError as e:
+            return Response({"message":f"Missing key : {str(e)}"} , status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserOrderBystatus(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        order_status = request.query_params.get('status')
+        user = request.user
+        orders = models.Order.objects.filter(user=user,delivery_status=order_status).order_by("-created_at")
+
+        serializer = serializers.OrderSerializer(orders,many=True)
+
+        return Response(serializer.data,status.HTTP_200_OK)
+
+class OrderDetails(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        order_id = request.query_params.get('id')
+        order = get_object_or_404(models.Order,id=order_id)
+        serializer = serializers.OrderSerializer(order)
+        return Response(serializer.data , status.HTTP_200_OK)
--- fashion-backend/project/settings.py
+++ fashion-backend/project/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'cart',
     'extra',
     'order',
+    'notification',
 ]
 
 MIDDLEWARE = [
